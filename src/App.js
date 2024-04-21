a20d335baa573824885c6ee061dac12126241843
wowhow234 <wauhauu@gmail.com>
2024-04-21 18:28:48 +0900

    edit css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import "./App.css";
+import "../src/css/app.css";
+import dummy from "./db/data.json";
 
 function App() {
   return (
     <div className="container">
-      <div className="item">box1</div>
+      <div className="title">
+        <span>Great Scenery!</span>
+        <p>모달창으로 이미지 보기 연습</p>
+        <div className="hr" />
+      </div>
+      {dummy.data.map((item, id) => (
+        <div key={id}>
+          <div className="item">
+            <div className="image">
+              <img src={item.image} alt={"pic" + `${item.title}`} />
+            </div>
+            <div className="picname">
+              <span>{item.title}</span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
