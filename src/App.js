560b75f062b80cdd524f92aed8feaec4b45f7d40
wowhow234 <wauhauu@gmail.com>
2024-04-24 00:13:56 +0900

    ㅠㅠ

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleopenModal = (id) => {
+    const dummydata = dummy.data[id - 1];
+    // if (id === dummydata.id) {
+    //   dummydata.imgState === true
+    //   setIsModalOpen(true);
+    //   console.log("id값 출력-----", id);
+    // } else console.log("----id 틀려용---", dummy.data[id - 1]);
+    // console.log("---if문 밖에서 id 출력---", id);
+    console.log("dummydata에서 id 추출", dummydata.id);
+    console.log("인자로 받은 id값 출력-----", id);
     setIsModalOpen(true);
-    console.log("id값 출력-----", id);
+    console.log("더미데이터배열목록", dummy.data);
+    console.log("isModalOpen?", isModalOpen);
   };
   const handlecloseModal = () => setIsModalOpen(false);
 
@@ -20,26 +30,24 @@ function App() {
         <div className="hr" />
       </div>
       {dummy.data.map((item, id) => (
-        <div key={id}>
-          <div className="item">
-            <div className="image">
-              <img
-                src={item.image}
-                alt={"pic" + `${item.title}`}
-                onClick={() => handleopenModal(item.id)}
-                key={item}
+        <div key={id} className="item">
+          <div className="image">
+            <img
+              src={item.image}
+              alt={"pic" + `${item.title}`}
+              onClick={() => handleopenModal(item.id)}
+            />
+
+            {isModalOpen && (
+              <Modal
+                // isOpen={isModalOpen}
+                closeModal={handlecloseModal}
+                data={item}
               />
-              {isModalOpen && (
-                <Modal
-                  isOpen={isModalOpen}
-                  closeModal={handlecloseModal}
-                  data={item}
-                />
-              )}
-            </div>
-            <div className="picname">
-              <span>{item.title}</span>
-            </div>
+            )}
+          </div>
+          <div className="picname">
+            <span>{item.title}</span>
           </div>
         </div>
       ))}
